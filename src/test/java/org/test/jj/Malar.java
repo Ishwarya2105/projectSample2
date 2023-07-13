8a12cf264930d9c321bcc9302ab56cc3c73bd2e0
Ishwarya2105 <ishwaryaishu2199@gmail.com>
2023-07-13 19:34:48 +0530

    Malar s code

---
+++ src/test/java/org/test/jj/Malar.java
@@ -0,0 +1,9 @@
+package org.test.jj;
+
+public class Malar {
+public static void main(String[] args) {
+	System.out.println("Test1");
+	System.out.println("test2");
+	System.out.println("test3");
+}
+}
